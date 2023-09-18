30f7840255174ccdc363ccb37cf7dee2baeaa981
S0beit18 <jatekadi@gmail.com>
2023-09-18 10:43:41 +0200

    Javított kód

--- main.cpp
+++ main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
-
 constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; 							//NELEMENTS --> hiányzik egy "_"
-    std::cout << '1-100 ertekek duplazasa' 					//";" hiányzik, std::endl, kettős aposztóf
-    for (int i = 0;) 										//nem teljes a ciklus, és ne 0-99ig írja ki, hanem 1-100ig
+    int b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+
+    for (int i = 1; i<=N_ELEMENTS+1; i++)
     {
-        b[i] = i * 2;
+        b[i] = i 2;
     }
-    for (int i = 0; i; i++)									//ciklus hibás "i<=N_ELEMENTS"
+
+    for (int i = 1; i<=N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" 								//hiányzik az érték --> "b[i]"
-    }    
+        std::cout << "Ertek:" << b[i] << std::endl;
+    }
+
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)				 	// "," helyett ";" a ciklusban, "i<=N_ELEMENTS"
+    for (int i = 1; i <= N_ELEMENTS; i++)
     {
-        atlag += b[i] 										//hiányzik a ";"
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
